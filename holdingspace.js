1056d93ad317e49bee14fedf0298097cec015851
Josh Langner <josh.langner@gmail.com>
2022-06-23 20:59:57 -0500

    add first and second answers

--- holdingspace.js
+++ holdingspace.js
@@ -1,7 +1,6 @@
 let name = prompt(`Tell me your name.`)
 alert(`Hello, ${name}, welcome to Hawkins.`)
 const introQuestion = prompt(`Are you a stranger things fan ${name}?`)
-
 if (introQuestion === 'Yes') {
   alert(
     `Ah! A true fan! Our friends in Hawkins are in danger, and you are the perfect one to help!`
@@ -24,31 +23,29 @@ if (firstChoice === 'Vecna') {
   if (partnerVecna === 'Steve') {
     const steveFriends = prompt(
       `You chose ${partnerVecna}. Strong Choice. How many friends are you bringing on the fight?`
-    )
-    if (parseInt(steveFriends) < 1) {
-      alert(
-        `OH NO, ${name}, You didn't bring any backup. Vecna killed you and Steve!`
-      )
-    } else {
-      alert(
-        `Good job! You and steve picked ${steveFriends} friends, and have defeated Vecna!`
-      )
-    }
-  }
+    )}
 
-  // partnerVecna === Robin
-} else if (partnerVecna === 'Robin') {
-  const robinFriends = prompt(
-    `You chose ${partnerVecna}. Smart Choice. How many friends are you bringing on the fight?`
+    // partnerVecna === Robin
+  } else if (partnerVecna === 'Robin') {
+    const robinFriends = prompt(
+      `You chose ${partnerVecna}. Smart Choice. How many friends are you bringing on the fight?`
+    )
+
+    // partnerVecna === INVALID RESPONSE
+  } else {
+    alert(`That was not an option. Vecna got you! The game will now restart.`)
+    location.reload()
+    //I found this location.reload() command on https://www.w3schools.com/jsref/met_loc_reload.asp
+    //I didn't know if it would do what I wanted, but it did!
+
+  //firstChoice === Russians
+} else if (firstChoice === 'Russians') {
+  const partnerRussia = prompt(
+    `Interesting choice, ${name}. Who do you want to fight with? Murray or Hopper?`
   )
-  if (parseInt(robinFriends) < 1) {
-    alert(
-      `OH NO, ${name}, You didn't bring any backup. Vecna killed you and Robin!`
-    )
-  }
-
-  // partnerVecna === INVALID RESPONSE
 } else {
-  alert(`That was not an option. Vecna got you! The game will now restart.`)
+  alert(
+    `That was not an option. The Russians got you! The game will now restart.`
+  )
   location.reload()
 }
