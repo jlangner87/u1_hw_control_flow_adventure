c5175388a0d12f0c2a57a1434638f01b9f6d0b8b
Josh Langner <josh.langner@gmail.com>
2022-06-23 22:00:31 -0500

    add more choices

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let name = prompt(`Tell me your name.`)
 alert(`Hello, ${name}, welcome to Hawkins.`)
-const introQuestion = prompt(`Are you a stranger things fan ${name}?`)
+const introQuestion = prompt(`Are you a stranger things fan, ${name}?`)
 
 if (introQuestion === 'Yes') {
   alert(
-    `Ah! A true fan! Our friends in Hawkins are in danger, and you are the perfect one to help!`
+    `Ah, good! A true fan! Our friends in Hawkins are in danger, and you are the perfect one to help!`
   )
 } else if (introQuestion === 'No') {
   alert(`Really? Okay... Hawkins is in danger, and they need your help anyway.`)
@@ -61,8 +61,52 @@ if (firstChoice === 'Vecna') {
     alert(`Murray is pretty cool, but you just died in a plane crash...`)
     location.reload()
   } else if (partnerRussia === 'Hopper') {
-    const hopperDoes = prompt(
+    const hopperEats = prompt(
       `You chose Hopper! Great choice! He has been in captivity and needs to eat. What should he eat?`
     )
+    switch (hopperEats) {
+      case 'peanut butter':
+        alert(
+          `${hopperEats} was just the right food for Hopper! He was replentished. You defeated the Russians and saved Hawkins!`
+        )
+        break
+      case 'Peanut butter':
+        alert(
+          `${hopperEats} was just the right food for Hopper! He was replentished. You defeated the Russians and saved Hawkins!`
+        )
+        break
+      case 'PEANUT BUTTER':
+        alert(
+          `${hopperEats} was just the right food for Hopper! He was replentished. You defeated the Russians and saved Hawkins!`
+        )
+        break
+      case 'Waffles':
+        alert(
+          `${hopperEats} was just the right food for Hopper! He was replentished. You defeated the Russians and saved Hawkins!`
+        )
+        break
+      case 'Eggos':
+        alert(
+          `${hopperEats} was just the right food for Hopper! He was replentished. You defeated the Russians and saved Hawkins!`
+        )
+        break
+      case 'Eggo Waffles':
+        alert(
+          `${hopperEats} was just the right food for Hopper! He was replentished. You defeated the Russians and saved Hawkins!`
+        )
+        break
+      default:
+        alert(
+          `${hopperEats} made Hopper sick. You were both captured and will live the rest of your lives in a top-secret Russian prison. Darn!`
+        )
+        location.reload()
+    }
   }
+}
+
+const playAgain = prompt(`Do you want to play again?`)
+if (playAgain === 'Yes') {
+  location.reload()
+} else {
+  alert(`FINE! I DIDNT WANT YOU TO PLAY AGAIN ANYWAY!`)
 }
