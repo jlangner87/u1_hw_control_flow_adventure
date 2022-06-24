9ef453a75d477045c9f62a66d0af1a6e6a1204a6
Josh Langner <josh.langner@gmail.com>
2022-06-23 21:42:22 -0500

    add russian choices

--- script.js
+++ script.js
@@ -14,13 +14,11 @@ if (introQuestion === 'Yes') {
 
 const firstChoice = prompt(`${name}, do you want to fight Vecna or Russians?`)
 
-// firstChoice === Vecna
 if (firstChoice === 'Vecna') {
   const partnerVecna = prompt(
-    `You are very brave, ${name}, will you be going to the Usidedown with Steve or Robin?`
+    `You are very brave, ${name}, will you be going to the Upsidedown with Steve or Robin?`
   )
 
-  // partnerVecna === Steve
   if (partnerVecna === 'Steve') {
     const steveFriends = prompt(
       `You chose ${partnerVecna}. Strong Choice. How many friends are you bringing on the fight?`
@@ -29,26 +27,42 @@ if (firstChoice === 'Vecna') {
       alert(
         `OH NO, ${name}, You didn't bring any backup. Vecna killed you and Steve!`
       )
+      location.reload()
     } else {
       alert(
-        `Good job! You and steve picked ${steveFriends} friends, and have defeated Vecna!`
+        `Good job! You and Steve picked ${steveFriends} friends, and have defeated Vecna!`
       )
     }
-  }
-
-  // partnerVecna === Robin
-} else if (partnerVecna === 'Robin') {
-  const robinFriends = prompt(
-    `You chose ${partnerVecna}. Smart Choice. How many friends are you bringing on the fight?`
-  )
-  if (parseInt(robinFriends) < 1) {
+  } else if (partnerVecna === 'Robin') {
+    const robinFriends = prompt(
+      `You chose smart! How many friends will you and ${partnerVecna} bring to fight Vecna?`
+    )
+    if (parseInt(robinFriends) < 1) {
+      alert(
+        `Bad nove, ${name}! You didn't bring any friends. Now you and Robin are stuck in the Upsidedown forever!`
+      )
+      location.reload()
+    } else {
+      alert(
+        `WOOHOO! You and Robin picked ${robinFriends}, and it was enough to finally defeat Vecna!`
+      )
+    }
+  } else {
     alert(
-      `OH NO, ${name}, You didn't bring any backup. Vecna killed you and Robin!`
+      `That was not an option. Vecna made you into a human pretzel! The game will now restart.`
+    )
+    location.reload()
+  }
+} else if (firstChoice === 'Russians') {
+  const partnerRussia = prompt(
+    `Interesting choice, ${name}... Will you be fighting the Russians with Hopper or Murray?`
+  )
+  if (partnerRussia === 'Murray') {
+    alert(`Murray is pretty cool, but you just died in a plane crash...`)
+    location.reload()
+  } else if (partnerRussia === 'Hopper') {
+    const hopperDoes = prompt(
+      `You chose Hopper! Great choice! He has been in captivity and needs to eat. What should he eat?`
     )
   }
-
-  // partnerVecna === INVALID RESPONSE
-} else {
-  alert(`That was not an option. Vecna got you! The game will now restart.`)
-  location.reload()
 }
